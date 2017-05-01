87e2990be0a9f06423a144aad69ef6379cd3c6dd
maespi <marcausejo@hotmail.com>
2017-05-01 22:18:08 +0200

    Update FiraBarcelona_AdministradorFira.sql

--- FiraBarcelona_AdministradorFira.sql
+++ FiraBarcelona_AdministradorFira.sql
@@ -25,10 +25,10 @@ DROP TABLE IF EXISTS `AdministradorFira`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `AdministradorFira` (
-  `codi` int(11) NOT NULL AUTO_INCREMENT,
-  `nomUsuari` varchar(45) DEFAULT NULL,
-  `password` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`codi`)
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `User` varchar(45) DEFAULT NULL,
+  `Passwd` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
