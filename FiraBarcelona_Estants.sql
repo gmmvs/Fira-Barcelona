7b1832fdb4295e20ffe731fee8a04a3b947dfe1f
maespi <marcausejo@hotmail.com>
2017-05-02 08:08:11 +0200

    Update FiraBarcelona_Estants.sql

--- FiraBarcelona_Estants.sql
+++ FiraBarcelona_Estants.sql
@@ -25,7 +25,7 @@ DROP TABLE IF EXISTS `Estant`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Estant` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `idEmpresa` int(11) DEFAULT NULL,
   `Nom` varchar(45) DEFAULT NULL,
   `NomEmpresa` varchar(45) DEFAULT NULL,
